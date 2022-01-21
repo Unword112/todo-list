09abbd0f04a8fead6741ac4781821992e0cba771
Unword112 <pog123@hotmail.co.uk>
2022-01-22 02:43:32 +0700

    'todolist'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
+//Data
+import data from './data.json'
+
+//components
+import Header from './Header'
+import ToDoForm from './ToDoForm';
+import ToDoList from './ToDoList'
+
 function App() {
+  const [ toDoList, setToDoList ] = useState(data)
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id == id ? { ...task, complete: true } : { ...task}
+    })
+    setToDoList(mapped)
+  }
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete
+    })
+    setToDoList(filtered)
+  }
+
+  const addTask = (userInput) => {
+    let copy = [...toDoList]
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false}]
+    setToDoList(copy)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+      <ToDoForm addTask={addTask} />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+
+export class Header extends Component {
+  render() {
+    return (
+      <View>
+        <h1>To Do List</h1>
+      </View>
+    );
+  }
+}
+
+export default Header;
